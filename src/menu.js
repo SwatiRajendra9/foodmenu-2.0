a613e2213cf1dd21a460ce499df67e3496db1c03
SwatiRajendra9 <swatirajendra09@gmail.com>
2023-03-20 16:50:38 +0530

    commit

---
+++ src/menu.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './App.css';
+
+
+function Menu(props) {
+  return(
+   
+    <div id='dish'>
+        
+        <img id='dish-image' src={props.categoryImg}></img>
+        
+        <div id='dish-info'>
+          <div id='name-price'>
+            <p id='dish-name'>{props.categoryName}</p>
+            <p id='dish-price'>{props.categoryPrice}</p>
+          </div>
+          <p id='dish-description'>{props.categoryDescription}</p>
+        </div>
+    </div>
+  )
+}
+export default Menu;
